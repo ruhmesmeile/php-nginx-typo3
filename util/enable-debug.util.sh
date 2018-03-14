ac065b351918c380f0a074ff131b6eadcbe7c13d
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2018-03-14 11:08:28 +0100

    Use environment variables for xdebug remote host and port, if present

--- util/enable-debug.util.sh
+++ util/enable-debug.util.sh
@@ -8,10 +8,21 @@ set -o errexit   ## set -e : exit the script if any statement returns a non-true
 
 pecl install -f xdebug-2.5.0;
 docker-php-ext-enable xdebug;
+
+if [ -z "$XDEBUG_REMOTE_HOST" ]; then
+  echo "xdebug.remote_host=$XDEBUG_REMOTE_HOST" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini;
+else
+  echo "xdebug.remote_host=127.0.0.1" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini;
+fi
+
+if [ -z "$XDEBUG_REMOTE_PORT" ]; then
+  echo "xdebug.remote_port=$XDEBUG_REMOTE_PORT" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini;
+else
+  echo "xdebug.remote_host=127.0.0.1" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini;
+fi
+
 echo "xdebug.max_nesting_level=500" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini;
 echo "xdebug.remote_enable=1" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini;
-echo "xdebug.remote_host=127.0.0.1" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini;
-echo "xdebug.remote_port=9000" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini;
 
 service php-fpm restart;
 
@@ -19,3 +30,9 @@ apt-get update;
 apt-get --yes install gdb strace;
 
 docker-image-cleanup;
+
+
+
+=192.168.100.172
+XDEBUG_REMOTE_PORT=9000
+XDEBUG_REMOTE_CONNECT_BACK=0
